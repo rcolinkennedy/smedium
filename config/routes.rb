c77d6081cf63a3cace692cface1c4eb410b6adaf
R. Colin Kennedy <c@rcolinkennedy.com>
2013-11-19 21:05:54 -0500

    Added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Smedium::Application.routes.draw do
+  root :to => "static_pages#home"
+
   get "static_pages/home"
+  get 'auth/twitter/callback', to: 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
