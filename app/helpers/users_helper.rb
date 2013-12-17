a92251f212c1f5a6f18911b9316acdb793e79671
R. Colin Kennedy <c@rcolinkennedy.com>
2013-12-17 13:55:28 -0500

    User Portrait pulling from Twitter Image, allows multiple sizes to be specified. Right now Views set to display 'bigger' size, but can make portrait rendered client-dependent

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,43 @@
 module UsersHelper
 
-# Returns the Gravatar (http://gravatar.com/) for the given user.
-  def portrait_for(user)
-    image_url = user.image
-    image_tag(image_url, alt: user.nickname, class: "gravatar")
+# Returns the Twitter image for the given user.
+  def portrait_for(user, size)
+    
+    # Twitter
+    # mini (24x24)                                                                  
+    # normal (48x48)                                            
+    # bigger (73x73)                                                
+    # original (variable width x variable height)
+
+    if user.image.include? "twimg"
+
+      # determine filetype        
+      case 
+      when user.image.downcase.include?(".jpeg")
+          filetype = ".jpeg"
+      when user.image.downcase.include?(".jpg")
+          filetype = ".jpg"
+      when user.image.downcase.include?(".gif")
+          filetype = ".gif"
+      when user.image.downcase.include?(".png")
+          filetype = ".png"
+      else
+          raise "Unable to read filetype of Twitter image for User ##{self.id}"
+      
+      end
+    end
+
+        # return requested size - code not used yet
+    #     if size == "original"
+    #         return self.image
+    #     else
+    #         return .image.gsub(filetype, "_#{size}#{filetype}")
+    #     end
+
+    # end
+
+    image_url = user.image.gsub(filetype, "_#{size}#{filetype}")
+    image_tag(image_url, alt: user.nickname, class: "portrait")
   end
 
 end
