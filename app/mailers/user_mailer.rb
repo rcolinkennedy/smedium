8f683d0134efce0e4ae538a6138e6dcc0a38ceb7
R. Colin Kennedy <c@rcolinkennedy.com>
2014-01-09 15:51:44 -0500

    Mandrill works over SMTP.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ActionMailer::Base
   default from: "smedium@rcolinkennedy.com"
 
   def email_confirm(user)
-    # Update the function to use Current User?
     @user = user
     mail :to => user.email, :subject => "Is this #{user.nickname}? Confirm your email"
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,16 @@ Smedium::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   # Use Letter Opener to configure transactional email
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
+
+  # Config for testing Mandrill
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 587,
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password  => ENV["MANDRILL_API_KEY"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
